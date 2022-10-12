9fb2926e314904b3c05dd7af2d895ba70c169c6e
Wannes Lambeens <Wannes.lambeens@student.ehb.be>
2022-10-12 10:44:36 +0200

    2-1-morelines finished + more efficient

--- exercises/2-variables/2-1-morelines.js
+++ exercises/2-variables/2-1-morelines.js
@@ -9,25 +9,26 @@ let context = canvas.getContext('2d');
 let width = context.canvas.width
 let height = context.canvas.height
 let margin = 100
+let step = 100
 
 
 drawMoreLines();
 
 function drawMoreLines(){
     context.beginPath();
-    context.moveTo(width/2 - margin, 100);
-    context.lineTo(width/2 + margin ,100);
+    context.moveTo(width/2 - margin, step);
+    context.lineTo(width/2 + margin , step);
 
-    context.lineTo(width/2 - margin, 200);
-    context.lineTo(width/2 + margin, 200);
-    context.lineTo(width/2 - margin, 300);
-    context.lineTo(width/2 + margin, 300);
-    context.lineTo(width/2 - margin, 400);
-    context.lineTo(width/2 + margin ,400);
-    context.lineTo(width/2 - margin, 500);
-    context.lineTo(width/2 + margin, 500);
-    context.lineTo(width/2 - margin, 600);
-    context.lineTo(width/2 + margin, 600);
+    context.lineTo(width/2 - margin, step*2);
+    context.lineTo(width/2 + margin, step*2);
+    context.lineTo(width/2 - margin, step*3);
+    context.lineTo(width/2 + margin, step*3);
+    context.lineTo(width/2 - margin, step*4);
+    context.lineTo(width/2 + margin ,step*4);
+    context.lineTo(width/2 - margin, step*5);
+    context.lineTo(width/2 + margin, step*5);
+    context.lineTo(width/2 - margin, step*6);
+    context.lineTo(width/2 + margin, step*6);
 
     context.lineTo(width/2 - margin, 100);
     context.stroke();
