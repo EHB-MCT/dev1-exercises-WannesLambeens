976473f2ea368ae4b510ea57ee51fbcfaf613127
Wannes Lambeens <Wannes.lambeens@student.ehb.be>
2022-11-09 23:32:58 +0100

    maze 1 finished

---
+++ exercises/5-selection-input-events/6-2-maze/6-2-1-maze.js
@@ -0,0 +1,28 @@
+"use strict"
+import context from "../../../scripts/context.js"
+import * as Utils from "../../../scripts/utils.js"
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+let value = 10;
+
+drawMaze();
+function drawMaze(){
+    for(let j = 0; j < width/10; j++){
+        for(let i = 0; i < height/10; i++){
+            let way = Math.floor(Math.random()*2);
+            console.log(way);
+            let value = 10;
+            let stepi = 10*i;
+            let stepj = 10*j;
+
+            context.lineWidth = "5";
+
+            if(way == 0){
+                Utils.drawLine(0 + stepj, 0 + stepi, 10 + stepj, 10 + stepi);
+            }else{
+                Utils.drawLine(10 + stepj, 0 + stepi, 0 + stepj, 10 + stepi);
+            }
+        }
+    }
+}
